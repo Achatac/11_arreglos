399d59b224f658c6e28d1f8e07c7839a83c3a871
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:44:20 -0500

    implementacion del for para imprimir y correccion de errores

--- arreglo2.cpp
+++ arreglo2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main (){
-    int CantDatos, datos[60], pares[30], impares[30], contPares, contImpares;
+    int CantDatos, datos[60], pares[30], impares[30], contPares = 0, contImpares = 0;
     cout << "Ingrese la cantidad de datos: "; cin >> CantDatos;
     for (int i=0; i < CantDatos; i++){
         cout << "Ingrese el dato: "; cin >> datos [i];
@@ -15,4 +15,13 @@ int main (){
             contImpares++;
         }
     }
+    cout << "La cantidad de pares es: " << contPares << endl;
+    for (int j=0; j < contPares; j++){
+        cout << pares[j] << " ";
+    }
+    cout << endl << "La cantidad de impares es: "<< contImpares << endl;
+    for (int k=0; k < contImpares; k++){
+        cout << impares[k] << " ";
+    }
+    return 0;
 }
