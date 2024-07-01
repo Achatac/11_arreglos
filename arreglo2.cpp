7652329455aa79c6f161d891b6061015981537df
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:30:46 -0500

    declaramos los arreglos

--- arreglo2.cpp
+++ arreglo2.cpp
@@ -1,1 +1,7 @@
-arreglos de pares e impares 
+//arreglos de pares e impares 
+#include <iostream>
+using namespace std;
+int main (){
+    int CantDatos, pares[30], impares[30], contPares, contImpares;
+    cout << "Ingrese la cantidad de datos: "; cin >> CantDatos;
+}
