d38d739bd17c3a4b360ec0a1fae4e36157171697
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:06:35 -0500

    se modifico el programa

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -2,20 +2,25 @@
 #include <iostream>
 using namespace std;
 
-float promedio(int cantidad[], int canN){
-    float promedio = 0;
+int sumatoria(int cantidad[], int canN);
+int porcentaje(int cantidad[]);
+
+int sumatoria(int cantidad[], int canN){
+    int sumatoria = 0;
     for (int i = 0; i < canN; i++){
-        promedio += cantidad[i];
+        sumatoria += cantidad[i];
     }
-    return promedio/canN;
-}
+    return sumatoria;
 
 int main (){
-    int cantidad[0], canN, numA;
+    int canN, numA;
     cout << "ingrese la cantidad de valores del arreglo: "; cin >> canN;
+    int cantidad[canN];
     for (int i = 0; i < canN; i++){
         cout << "ingrese un valor del arreglo: "; cin >> numA;
         cantidad[i] = numA;
     }
+    promedio (cantidad, canN);
+
     return 0;
 }
