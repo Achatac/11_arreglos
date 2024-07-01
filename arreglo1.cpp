b8343b86cef4d2d76ccd0b8deddee28158864396
Achatac <achatac@unjbg.edu.pe>
2024-07-01 09:44:55 -0500

    se agrego y modifico otras variables

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 int main (){
-    int cantidad[..], numSolicitados;
-    cout << "ingrese la cantidad de valores del arreglo: "; cin >> numSolicitados;
-    for (int i = 0; i < numSolicitados; i++){
+    int cantidad[..], canN, numA;
+    cout << "ingrese la cantidad de valores del arreglo: "; cin >> canN;
+    for (int i = 0; i < canN; i++){
 
     }
     return 0;
