6b8fe45f5cd40bf63bd9fad46c4acd3d8c06798f
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:15:57 -0500

    implementamos la estructura if

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 using namespace std;
 int main (){
-    int sumatoria, porcentaje, CantDatos;
+    int sumatoria, porcentaje, CantDatos, ceros;
+    ceros = 0;
     cout << "Ingrese la cantidad de datos: "; cin >> CantDatos;
     int arreglo[CantDatos];
     for (int i=0; i < CantDatos; i++){
-
+        cout << "Ingrese el dato "; cin >> arreglo [i];
+        if (arreglo[i] != 0){
+            sumatoria += arreglo[i];
+        } else {
+            
+        }
     }
 }
