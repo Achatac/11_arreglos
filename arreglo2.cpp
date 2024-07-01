a0754ab2737b82fa734d2ca7feb577cfbf6dd15f
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:32:38 -0500

    anadimos la estructura for

--- arreglo2.cpp
+++ arreglo2.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 using namespace std;
 int main (){
-    int CantDatos, pares[30], impares[30], contPares, contImpares;
+    int CantDatos, datos[60], pares[30], impares[30], contPares, contImpares;
     cout << "Ingrese la cantidad de datos: "; cin >> CantDatos;
+    for (int i=0; i < CantDatos; i++){
+        cout << "Ingrese el dato: "; cin >> datos [i];
+    }
 }
