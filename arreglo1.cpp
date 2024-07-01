da847eadb86d4bd4b9ad6c52b3f01835917ec95e
Achatac <achatac@unjbg.edu.pe>
2024-07-01 09:41:38 -0500

    implementamos la estructura for

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -2,5 +2,10 @@
 #include <iostream>
 using namespace std;
 int main (){
+    int cantidad[..], numSolicitados;
+    cout << "ingrese la cantidad de valores del arreglo: "; cin >> numSolicitados;
+    for (int i = 0; i < numSolicitados; i++){
+
+    }
     return 0;
 }
