f2be101c93024c7902a3a97a611fcf8d42a399b4
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:12:19 -0500

    se implemento la estructura for

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -1,1 +1,11 @@
-sumatoria y porcentaje 
+//sumatoria y porcentaje 
+#include <iostream>
+using namespace std;
+int main (){
+    int sumatoria, porcentaje, CantDatos;
+    cout << "Ingrese la cantidad de datos: "; cin >> CantDatos;
+    int arreglo[CantDatos];
+    for (int i=0; i < CantDatos; i++){
+
+    }
+}
