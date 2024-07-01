c97f75404de5c12af70ac79177bcd3f930d10de5
Achatac <achatac@unjbg.edu.pe>
2024-07-01 10:34:28 -0500

    anadimos la estructura if, para saber si es par o no

--- arreglo2.cpp
+++ arreglo2.cpp
@@ -1,10 +1,18 @@
 //arreglos de pares e impares 
 #include <iostream>
 using namespace std;
+
 int main (){
     int CantDatos, datos[60], pares[30], impares[30], contPares, contImpares;
     cout << "Ingrese la cantidad de datos: "; cin >> CantDatos;
     for (int i=0; i < CantDatos; i++){
         cout << "Ingrese el dato: "; cin >> datos [i];
+        if (datos[i] % 2 == 0){
+            pares[contPares] = datos[i];
+            contPares++;
+        } else {
+            impares[contImpares] = datos[i];
+            contImpares++;
+        }
     }
 }
