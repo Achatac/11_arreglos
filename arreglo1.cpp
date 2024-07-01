b0d8c8323cfbb0765d29c2ffcffc34f3427ba43c
Achatac <achatac@unjbg.edu.pe>
2024-07-01 09:51:47 -0500

    anadimos una funcion para el promedio

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -1,11 +1,16 @@
 //almecenar elementos 
 #include <iostream>
 using namespace std;
+
+float promedio(int cantidad[], int canN){
+}
+
 int main (){
-    int cantidad[..], canN, numA;
+    int cantidad[0], canN, numA;
     cout << "ingrese la cantidad de valores del arreglo: "; cin >> canN;
     for (int i = 0; i < canN; i++){
-
+        cout << "ingrese un valor del arreglo: "; cin >> numA;
+        cantidad[i] = numA;
     }
     return 0;
 }
