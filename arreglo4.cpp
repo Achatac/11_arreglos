f30d350974cf5542f43e647b5470f6fd4aa38d08
Achatac <achatac@unjbg.edu.pe>
2024-07-05 18:33:24 -0500

    agregamos la estructura for dentro de la funcion relleno

--- arreglo4.cpp
+++ arreglo4.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int relleno(int arreglo[], int tam);
 
 int relleno(int arreglo[], int tam){
-
+    int mult;
+    cout << "Ingrese el multiplo dentro del arreglo: "; cin >> mult;
+    for (int i = 0; i < tam; i++ ){
+        arreglo[i] = mult * (i+1);
+    }
 }
 
 int main (){
