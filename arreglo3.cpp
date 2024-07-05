6994f354d7c9a9fe949b55e03296f84861b0023e
Achatac <achatac@unjbg.edu.pe>
2024-07-05 16:36:59 -0500

    correccion de las funciones

--- arreglo3.cpp
+++ arreglo3.cpp
@@ -3,12 +3,22 @@
 using namespace std;
 
 void Media (float temMax[],float temMin[]);
-
-void menorT (float temMedia[]);
-
+void diaM (float temMax[],float temMin[]);
 void TemPorT (float temMax[]);
 
-int main {
+void Media (float temMax[],float temMin[]){
+
+}
+void diaM (float temMax[],float temMin[]){
+
+}
+void TemPorT (float temMax[]){
+    
+}
+
+
+
+int main (){
     float temMax[7], temMin[7], temMedia[7];
     return 0;
-}d
+}
