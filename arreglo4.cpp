d0100cc43d10c1206ea0c51287fda053cefde572
Achatac <achatac@unjbg.edu.pe>
2024-07-05 18:29:43 -0500

    anadimos la funcion

--- arreglo4.cpp
+++ arreglo4.cpp
@@ -1,1 +1,13 @@
-multiplos de un numero 
+//multiplos de un numero 
+#include <iostream>
+using namespace std;
+
+int relleno(int arreglo[], int tam);
+
+int relleno(int arreglo[], int tam){
+
+}
+
+int main (){
+    return 0;
+}
