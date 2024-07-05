356057fac86aec1bb392214032ce97cce316a5ce
Achatac <achatac@unjbg.edu.pe>
2024-07-05 18:09:02 -0500

    implemento de la estructura for, ademas de eliminar una funcion

--- arreglo3.cpp
+++ arreglo3.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 float Media (float temMax,float temMin);
 int diaM (float temMedia[]);
-void TemPorT (float temMax);
 
 float Media (float temMax, float temMin){
     return (temMax + temMin)/2.0;
@@ -20,24 +19,38 @@ int diaM(float temMedia[]) {
     return M;
 }
 
-void TemPorT (float temMax[]){
-
-}
-
-
-
 int main (){
-    float temMax[7], temMin[7], temMedia[7];
-    string dias[7] = {"Lunes", "Martes", "Miercoles","Jueves", "Viernes", "Sabado", "Domingo"};
+    float temMax[7], temMin[7], temMedia[7], tem;
+    string dias[7] = {"Lunes", "Martes", "Miercoles","Jueves", "Viernes", "Sabado", "Domingo"} , diasMax[7];
     for (int i = 0; i < 7; i++){
         cout << "Ingrese la temperatura minima del " << dias[i] << " : "; cin >> temMin[i];
         cout << "Ingrese la temperatura maxima del " << dias[i] << " : "; cin >> temMax[i];
     }
+
     for (int j = 0; j < 7; j++){
         temMedia[j] = Media(temMax[j], temMin[j]);
         cout << "La temperatura media del " << dias[j] << " es:  " << temMedia[j] << endl;
     }
+
     int Dmin = diaM(temMedia);
-    cout << "El dia con la temperatura minima es: " << dias[Dmin];
+    cout << "El dia con la temperatura minima es: " << dias[Dmin] <<endl;
+    cout << "Ingrese por teclado la temperatura: "; cin >> tem;
+    int c = 0;
+    for (int w = 0; w < 7; w++){
+        if (tem == temMax[w]){
+            diasMax[0] = dias[w];
+            int c = 0;
+            c++;
+        }
+    }
+    if (c > 0){
+        cout << "Los dias con temperatura maxima que coinciden es/son: ";
+        for (int e = 0; e < 7; e++){
+            cout << diasmax[e] << "  ";
+        }
+    }
+    else {
+        cout << "No existe ningun dia con temperatura maxima que coincida" ;
+    }
     return 0;
 }
