4139ce824dac3ca32878f7f34b1cf68eccbaa745
Achatac <achatac@unjbg.edu.pe>
2024-07-05 16:40:55 -0500

    se anadio un arreglo string y la estructura for

--- arreglo3.cpp
+++ arreglo3.cpp
@@ -1,5 +1,6 @@
 //temperatura minima y maxima 
 #include <iostream> 
+#include <string>
 using namespace std;
 
 void Media (float temMax[],float temMin[]);
@@ -13,12 +14,17 @@ void diaM (float temMax[],float temMin[]){
 
 }
 void TemPorT (float temMax[]){
-    
+
 }
 
 
 
 int main (){
     float temMax[7], temMin[7], temMedia[7];
+    string[7] = {"Lunes", "Martes", "Miercoles","Jueves", "Viernes", "Sabado", "Domingo"}
+    for (int i = 0; i < 7; i++){
+        
+    }
+
     return 0;
 }
