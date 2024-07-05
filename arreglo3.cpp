1fe18b51897e017ff0db7aab9e09bc5bb6263c05
Achatac <achatac@unjbg.edu.pe>
2024-07-05 16:33:57 -0500

    implementacion de las funciones y arreglos

--- arreglo3.cpp
+++ arreglo3.cpp
@@ -1,1 +1,14 @@
-temperatura minima y maxima 
+//temperatura minima y maxima 
+#include <iostream> 
+using namespace std;
+
+void Media (float temMax[],float temMin[]);
+
+void menorT (float temMedia[]);
+
+void TemPorT (float temMax[]);
+
+int main {
+    float temMax[7], temMin[7], temMedia[7];
+    return 0;
+}d
