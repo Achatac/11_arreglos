00cfa2a9f476d4ae03e8740da1545f2bc94c6fe1
Achatac <achatac@unjbg.edu.pe>
2024-07-05 19:00:39 -0500

    anadimos la estructura if para evitar el ingreso de un arreglo menor a cero o mayor a 100

--- arreglo4.cpp
+++ arreglo4.cpp
@@ -11,13 +11,18 @@ void relleno(int arreglo[], int tam, int mult){
 } 
 
 int main (){
-    int arreglo[100], tam;
-    cout << "Ingrese el tamano del arreglo: "; cin >> tam;
-    cout << "Ingrese el multipplo del arreglo: "; cin >> mult;
+    int arreglo[100], tam, mult;
+    cout << "Ingrese el tamano del arreglo (max 100): "; cin >> tam;
+    cout << "Ingrese el multiplo del arreglo: "; cin >> mult;
+    if (tam > 0 && tam <= 100){
     relleno(arreglo,tam,mult);
     cout << "El arreglo contiene los elementos: ";
     for (int j = 0; j < tam; j++ ){
         cout << arreglo[j] << "  ";
+    }
+    }
+    else {
+        cout << "El tamano del arreglo debe ser mayor que 0 y menor o igual a 100";
     }
     return 0;
 }
