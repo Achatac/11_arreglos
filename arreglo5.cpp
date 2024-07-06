213c862902cd3aa77f7ee48d7cbf5f72bdfe15ec
Achatac <achatac@unjbg.edu.pe>
2024-07-05 19:54:50 -0500

    implementamos la estructura for para implementarlas dentro del arreglo NomC y tambien evitamos la repeticion de nombres con break

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -4,15 +4,29 @@
 using namespace std;
 int main (){
     int tamPG, tamFP, tamC;
-    string PG[100]; FP[100], NomC[100];
+    string PG[100];
+    string FP[100]; 
+    string NomC[100];
     cout << "Ingrese la cantidad de estudiantes de FP: "; cin >> tamFP;
     cout << "Ingrese los nombres de los estudiates de FP: " << endl;
     for (int i = 0; i < tamFP; i++){
         cin >> FP[i];
     }
+
     cout << "Ingrese la cantidad de estudiantes de PG: "; cin >> tamPG;
     cout << "Ingrese los nombres de los estudiates de PG: " << endl;
     for (int j = 0; j < tamPG; j++){
         cin >> PG[j];
     }
+
+    tamC = 0;
+    for (int k = 0; k < tamFP;++k) {
+        for (int w = 0; w < tamPG; ++w) {
+            if (FP[k] == PG[w]) {
+                NomC[tamC++] = FP[k];
+                break;
+            }
+        }
+    }
+    return 0;
 }
