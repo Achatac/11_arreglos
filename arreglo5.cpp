37ce7ba909352bc85414d401761d135827e9474e
Achatac <achatac@unjbg.edu.pe>
2024-07-05 19:14:39 -0500

    declaramos las variables y arreglos

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -1,1 +1,10 @@
-nombres de estudiantes 
+//nombres de estudiantes 
+#include <iostream>
+#include <string>
+using namespace std;
+int main (){
+    int tamPG, tamFP, tamT;
+    string PG[100]; FP[100];
+    cout << "Ingrese la cantidad de estudiantes de FP: "; cin >> tamFP;
+    cout << "Ingrese la cantidad de estudiantes de PG: "; cin >> tamPG;
+}
