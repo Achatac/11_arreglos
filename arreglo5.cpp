e1829e9356e13d5fd4ff534f1f1a4835dc3b3494
Achatac <achatac@unjbg.edu.pe>
2024-07-05 19:18:32 -0500

    implementamos las estructuras for

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -3,8 +3,16 @@
 #include <string>
 using namespace std;
 int main (){
-    int tamPG, tamFP, tamT;
-    string PG[100]; FP[100];
+    int tamPG, tamFP, tamC;
+    string PG[100]; FP[100], NomC[100];
     cout << "Ingrese la cantidad de estudiantes de FP: "; cin >> tamFP;
+    cout << "Ingrese los nombres de los estudiates de FP: " << endl;
+    for (int i = 0; i < tamFP; i++){
+        cin >> FP[i];
+    }
     cout << "Ingrese la cantidad de estudiantes de PG: "; cin >> tamPG;
+    cout << "Ingrese los nombres de los estudiates de PG: " << endl;
+    for (int j = 0; j < tamPG; j++){
+        cin >> PG[j];
+    }
 }
